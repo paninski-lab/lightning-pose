538a382bbedbfaae369806f4eb5bdf1320d56000
Sunand Raghupathi <sunraghupathi@gmail.com>
2021-05-19 17:29:26 -0400

    heatmap loss init

--- pose_est_nets/datasets/datasets.py
+++ pose_est_nets/datasets/datasets.py
@@ -1,17 +1,22 @@
 import torch
 import pandas as pd
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 from PIL import Image
 from typing import Callable, Optional, Tuple, List
 import os
+import numpy as np
+from PIL import Image
 
-class TrackingDataset(torch.utils.data.Dataset):
-    def __init__(self,
-                 root_directory: str,
-                 csv_path: str,
-                 header_rows: Optional[List[int]] = None,
-                 transform: Optional[Callable] = None
-                 ) -> None:
+
+class RegressionDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        root_directory: str,
+        csv_path: str,
+        header_rows: Optional[List[int]] = None,
+        transform: Optional[Callable] = None,
+    ) -> None:
         """
         Initializes the Tracking Dataset
         Parameters:
@@ -24,8 +29,10 @@ class TrackingDataset(torch.utils.data.Dataset):
         Returns:
             None
         """
-        csv_data = pd.read_csv(os.path.join(root_directory, csv_path), header=header_rows)
-        self.image_names = list(csv_data.iloc[:,0])
+        csv_data = pd.read_csv(
+            os.path.join(root_directory, csv_path), header=header_rows
+        )
+        self.image_names = list(csv_data.iloc[:, 0])
         self.labels = torch.tensor(csv_data.iloc[:, 1:].to_numpy(), dtype=torch.float32)
         self.transform = transform
         self.root_directory = root_directory
@@ -38,7 +45,9 @@ class TrackingDataset(torch.utils.data.Dataset):
         # get img_name from self.image_names
         img_name = self.image_names[idx]
         # read image from file and apply transformations (if any)
-        x = Image.open(os.path.join(self.root_directory, img_name)).convert('RGB') # Rick's images have 1 color channel; change to 3.
+        x = Image.open(os.path.join(self.root_directory, img_name)).convert(
+            "RGB"
+        )  # Rick's images have 1 color channel; change to 3.
         if self.transform:
             x = self.transform(x)
 
@@ -46,3 +55,93 @@ class TrackingDataset(torch.utils.data.Dataset):
         y = self.labels[idx]
 
         return x, y
+
+
+class HeatmapDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        root_directory: str,
+        csv_path: str,
+        header_rows: Optional[List[int]] = None,
+        transform: Optional[Callable] = None,
+    ) -> None:
+        """
+        Initializes the Tracking Dataset
+        Parameters:
+            root_directory (str): path to data directory
+            csv_path (str): path to CSV file (within root_directory). CSV file should be
+                in the form (image_path, bodypart_1_x, bodypart_1_y, ..., bodypart_n_y)
+                Note: image_path is relative to the given root_directory
+            header_rows (List[int]): (optional) which rows in the csv are header rows
+            transform (torchvision.transforms): (optional) transform to apply to images
+        Returns:
+            None
+        """
+        csv_data = pd.read_csv(
+            os.path.join(root_directory, csv_path), header=header_rows
+        )
+        self.image_names = list(csv_data.iloc[:, 0])
+        self.labels = torch.tensor(csv_data.iloc[:, 1:].to_numpy(), dtype=torch.float32)
+        self.labels = torch.reshape(self.labels, (self.labels.shape[0], -1, 2))
+        self.transform = transform
+        self.root_directory = root_directory
+        self.num_targets = self.labels.shape[1]
+
+    def __len__(self) -> int:
+        return len(self.image_names)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.tensor, torch.tensor]:
+        # get img_name from self.image_names
+        img_name = self.image_names[idx]
+        # read image from file and apply transformations (if any)
+        x = Image.open(os.path.join(self.root_directory, img_name)).convert(
+            "RGB"
+        )  # Rick's images have 1 color channel; change to 3.
+        if self.transform:
+            x = self.transform(x)
+
+        # get labels from self.labels
+        y = self.labels[idx]
+        y_heatmap = torch.zeros((y.shape[0], x.shape[-2], x.shape[-1]))
+
+        # TODO: vectorize this operation
+        for bp_idx in range(y.shape[0]):
+            if not torch.any(torch.isnan(y[bp_idx])):
+                y_heatmap[bp_idx] = torch.from_numpy(
+                    self.gaussian(
+                        y_heatmap[bp_idx].detach().cpu().numpy(),
+                        y[bp_idx].detach().cpu().numpy(),
+                    )
+                )
+
+        return x, y_heatmap
+
+    def gaussian(self, img, pt, sigma=10):
+        # Draw a 2D gaussian
+
+        # Check that any part of the gaussian is in-bounds
+        ul = [int(pt[0] - 3 * sigma), int(pt[1] - 3 * sigma)]
+        br = [int(pt[0] + 3 * sigma + 1), int(pt[1] + 3 * sigma + 1)]
+        if ul[0] > img.shape[1] or ul[1] >= img.shape[0] or br[0] < 0 or br[1] < 0:
+            # If not, just return the image as is
+            return img
+
+        # Generate gaussian
+        size = 6 * sigma + 1
+        x = np.arange(0, size, 1, float)
+        y = x[:, np.newaxis]
+        x0 = y0 = size // 2
+        # The gaussian is not normalized, we want the center value to equal 1
+        g = np.exp(-((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2))
+
+        # Usable gaussian range
+        g_x = max(0, -ul[0]), min(br[0], img.shape[1]) - ul[0]
+        g_y = max(0, -ul[1]), min(br[1], img.shape[0]) - ul[1]
+        # Image range
+        img_x = max(0, ul[0]), min(br[0], img.shape[1])
+        img_y = max(0, ul[1]), min(br[1], img.shape[0])
+
+        img[img_y[0] : img_y[1], img_x[0] : img_x[1]] = g[
+            g_y[0] : g_y[1], g_x[0] : g_x[1]
+        ]
+        return img
---
+++ pose_est_nets/models/heatmap_tracker.py
@@ -0,0 +1,146 @@
+import torch
+import torchvision.models as models
+from torch.nn import functional as F
+from torch import nn
+from pytorch_lightning.core.lightning import LightningModule
+from torch.utils.data import DataLoader, random_split
+from torch.optim import Adam
+from typing import Any, Callable, Optional, Tuple, List
+import numpy as np
+
+
+class HeatmapTracker(LightningModule):
+    def __init__(
+        self,
+        num_targets: int,
+        dataset: str,
+        resnet_version: int = 18,
+        transfer: Optional[bool] = False,
+    ) -> None:
+        """
+        Initializes regression tracker model with resnet backbone
+        :param num_targets: number of body parts
+        :param resnet_version: The ResNet variant to be used (e.g. 18, 34, 50, 101, or 152). Essentially specifies how
+            large the resnet will be.
+        :param transfer:  Flag to indicate whether this is a transfer learning task or not; defaults to false,
+            meaning the entire model will be trained unless this flag is provided
+        """
+        super(HeatmapTracker, self).__init__()
+        self.__dict__.update(locals())  # todo: what is this?
+        resnets = {
+            18: models.resnet18,
+            34: models.resnet34,
+            50: models.resnet50,
+            101: models.resnet101,
+            152: models.resnet152,
+        }
+        # Using a pretrained ResNet backbone
+        backbone = resnets[resnet_version](pretrained=True)
+        num_filters = backbone.fc.in_features
+        layers = list(backbone.children())[:-1]
+        self.feature_extractor = nn.Sequential(*layers)
+
+        self.upsampling_layers = []
+        in_dim = num_filters // 16
+        out_dim = 64
+        for i in range(3):
+            self.upsampling_layers += [
+                nn.Conv2d(in_dim, out_dim, kernel_size=3, stride=1, padding=1),
+                nn.Upsample(scale_factor=2, mode="bilinear"),
+            ]
+            in_dim = out_dim
+
+        self.upsampling_layers += [
+            nn.Conv2d(in_dim, num_targets, kernel_size=3, stride=1, padding=1)
+        ]
+        self.upsampling_layers = nn.Sequential(*self.upsampling_layers)
+
+        self.dataset = dataset
+        self.batch_size = 16
+        self.num_workers = 0
+
+        # freeze all layers but the last
+        if transfer:
+            for child in list(self.feature_extractor.children())[:-1]:
+                for param in child.parameters():
+                    param.requires_grad = False
+            print("Froze all layers but the last.")
+
+    def setup(self, stage: Optional[str] = None):
+        datalen = self.dataset.__len__()
+        self.train_set, self.valid_set, self.test_set = random_split(
+            self.dataset,
+            [round(datalen * 0.7), round(datalen * 0.1), round(datalen * 0.2)],
+            generator=torch.Generator().manual_seed(42),
+        )
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        """
+        Forward pass through the network
+        :param x: input
+        :return: output of network
+        """
+        self.feature_extractor.eval()
+        with torch.no_grad():
+            representations = torch.reshape(
+                self.feature_extractor(x), (x.shape[0], -1, 4, 4)
+            )
+        out = self.upsampling_layers(representations)
+        upsample_final = nn.Upsample(size=(x.shape[-2], x.shape[-1]), mode="bilinear")
+        out = upsample_final(out)
+        return out
+
+    @staticmethod
+    def heatmap_loss(y: torch.tensor, y_hat: torch.tensor) -> torch.tensor:
+        """
+        Computes mse loss between ground truth (x,y) coordinates and predicted (x^,y^) coordinates
+        :param y: ground truth. shape=(num_targets, 2)
+        :param y_hat: prediction. shape=(num_targets, 2)
+        :return: mse loss
+        """
+        # apply mask
+        # compute loss
+        loss = F.mse_loss(y_hat, y)
+        return loss
+
+    def training_step(self, data, batch_idx):
+        x, y = data
+        # forward pass
+        y_hat = self.forward(x)
+        # compute loss
+        loss = self.heatmap_loss(y, y_hat)
+        # log training loss
+        self.log(
+            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return {"loss": loss}
+
+    def validation_step(self, data, batch_idx):
+        x, y = data
+        y_hat = self.forward(x)
+        # compute loss
+        loss = self.heatmap_loss(y, y_hat)
+        # log validation loss
+        self.log("val_loss", loss, prog_bar=True, logger=True)
+
+    def test_step(self, data, batch_idx):
+        self.validation_step(data, batch_idx)
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=1e-3)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_set, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.valid_set, batch_size=self.batch_size, num_workers=self.num_workers
+        )
+
+    def test_dataloader(self):
+        return DataLoader(self.test_set, batch_size=1, num_workers=self.num_workers)
+
+    # def predict(self, batch, batch_idx: int , dataloader_idx: int = None):
+
