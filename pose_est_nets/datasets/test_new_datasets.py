2e242ef6b97e8cf0a1d4ca0f4bff5f557878df96
Ubuntu <ubuntu@ip-172-31-72-121.ec2.internal>
2021-09-11 20:08:40 +0000

    refactored datasets, added tests, and moved draw_keypoints method to new dataset_utils file

--- pose_est_nets/datasets/test_new_datasets.py
+++ pose_est_nets/datasets/test_new_datasets.py
@@ -7,17 +7,31 @@ import shutil
 import imgaug.augmenters as iaa
 
 def test_heatmap_dataset():
-	from pose_est_nets.datasets.datasets import DLCHeatmapDataset
-	data_transform = []
-	data_transform.append(
-	    iaa.Resize({"height": 384, "width": 384})
-	)  # dlc dimensions need to be repeatably divisable by 2
-	imgaug_transform = iaa.Sequential(data_transform)
+        from pose_est_nets.datasets.datasets import BaseTrackingDataset, DLCHeatmapDataset
+        data_transform = []
+        data_transform.append(
+            iaa.Resize({"height": 384, "width": 384})
+        )  # dlc dimensions need to be repeatably divisable by 2
+        imgaug_transform = iaa.Sequential(data_transform)
 
-	regData = BaseTrackingDataset(root_directory="toy_datasets/toymouseRunningData", csv_path="CollectedData_.csv",
+        regData = BaseTrackingDataset(root_directory="../../toy_datasets/toymouseRunningData", csv_path="CollectedData_.csv",
                               header_rows=[1, 2], imgaug_transform = imgaug_transform)
-	heatmapData = DLCHeatmapDataset(root_directory="toy_datasets/toymouseRunningData", csv_path="CollectedData_.csv",
+        heatmapData = DLCHeatmapDataset(root_directory="../../toy_datasets/toymouseRunningData", csv_path="CollectedData_.csv",
                               header_rows=[1, 2], imgaug_transform = imgaug_transform, mode = 'csv')
+        assert(torch.equal(regData.__getitem__(0)[0], heatmapData.__getitem__(0)[0]))
+        assert(heatmapData.__getitem__(0)[0].shape[1:] == (heatmapData.height, heatmapData.width))
+        assert(heatmapData.__getitem__(0)[1].shape[1:] == heatmapData.output_shape)
+        assert(type(regData.__getitem__(0)[1]) == torch.Tensor)
+        numLabels = regData.labels.shape[1]
+        for idx in range(numLabels):
+            if torch.any(torch.isnan(regData.__getitem__(0)[1][idx])):
+                print(torch.max(heatmapData.__getitem__(0)[1][idx]))
+                assert(torch.max(heatmapData.__getitem__(0)[1][idx]) == torch.tensor(0))
+            else:
+                assert(torch.max(heatmapData.__getitem__(0)[1][idx]) != torch.tensor(0))
+        
+        print(heatmapData.__getitem__(11)[1])
+        
 
 
 test_heatmap_dataset()
