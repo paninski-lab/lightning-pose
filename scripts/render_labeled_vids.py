eaae99596eab5aab373c12d96844615f04f0be56
danbider <>
2021-10-27 21:45:09 +0000

    modified test vid to h264 and made fiftyone video plot
    Former-commit-id: 44f8b93506d78168d2ae2e3c7aa24923c8ab3e92

---
+++ scripts/render_labeled_vids.py
@@ -0,0 +1,58 @@
+import fiftyone as fo
+import pandas as pd
+import os
+import fiftyone.utils.video as fouv
+
+# TODO: make a dataset?
+dataset = fo.Dataset()
+
+video_path = "/home/jovyan/lightning-pose/toy_datasets/toymouseRunningData/unlabeled_videos/test_vid.mp4"
+
+"""if video_path is not codec h.264:
+video_path_transformed = "/home/jovyan/lightning-pose/toy_datasets/toymouseRunningData/unlabeled_videos/transformed_test_vid.mp4"
+fouv.reencode_video(video_path, video_path_transformed, verbose=False)
+"""
+
+assert os.path.isfile(video_path)
+
+video_sample = fo.Sample(filepath=video_path)
+print(video_sample)
+
+
+csv_with_preds = pd.read_csv(
+    "/home/jovyan/lightning-pose/toy_datasets/toymouseRunningData/unlabeled_videos/bla.csv",
+    header=[1, 2],
+)
+
+# TODO: make as params
+height = 406
+width = 396
+
+bodypart_names = csv_with_preds.columns.levels[0][1:]
+# TODO: check how to populate and use the confidence field
+for frame_idx in range(csv_with_preds.shape[0]):  # loop over frames
+    pred_list = []
+    for bp in bodypart_names:  # loop over bp names
+        # that's quick but looses body part name labels
+        pred_list.append(
+            (
+                csv_with_preds[bp]["x"][frame_idx] / width,
+                csv_with_preds[bp]["y"][frame_idx] / height,
+            )
+        )
+    # note that their indexing starts from 1
+    video_sample.frames[frame_idx + 1]["preds"] = fo.Keypoints(
+        keypoints=[fo.Keypoint(points=pred_list)]
+    )
+
+dataset.add_sample(video_sample)
+dataset.compute_metadata()
+# print(dataset)
+session = fo.launch_app(dataset, remote=True)
+session.wait()
+# inspect frames
+# for frame_number, frame in video_sample.frames.items():
+#     print(frame)
+#     break
+
+# now loop over the rows of the csv with prediction. add frame information as frame["predictions"]
