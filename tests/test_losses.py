7090cae63617f3ebc40a67cca10b08edb429c736
Nick Greenspan <nicholasg101@gmail.com>
2021-11-05 22:12:07 -0400

    fixed typo in test_losses

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -126,7 +126,7 @@ def test_SingleView_PCA_loss():
         discarded_eigenvectors=discarded_evecs,
         epsilon=epsilon
     )
-    assert(single_view_pca_loss == 0.0
+    assert(single_view_pca_loss == 0.0)
 
 
 
