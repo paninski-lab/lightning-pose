9d5f181927685efa9049ae0d4dc61806a4b5f26d
nickgreenspan <nicholasg101@gmail.com>
2021-11-07 18:08:55 +0000

    wrote and tested torch implemenation of draw_keypoints, batch functionality testing to follow
    Former-commit-id: fb82b417785702d6eb9c50c8572a7ea3deccf793

--- pose_est_nets/utils/dataset_utils.py
+++ pose_est_nets/utils/dataset_utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 import torch
 from torchtyping import TensorType, patch_typeguard
+from typeguard import typechecked
 import math
+#import inspect
 
 patch_typeguard()  # use before @typechecked
 
@@ -34,41 +36,35 @@ def draw_keypoints(keypoints, height, width, output_shape, sigma=1, normalize=Tr
 #batched implementation of above numpy function using torch
 #assumes keypoints are detached, or doesn't really matter because we are doing it here
 @typechecked
-def draw_keypoints_torch(
+def generate_heatmaps(
     keypoints: TensorType["batch", "num_keypoints", 2], 
     height: int, #height of full sized image
     width: int,  #width of full sized image
     output_shape: tuple, #dimensions of full sized image
-    sigma: int = 1, #sigma used for generating heatmaps
+    sigma: int = 1.25, #sigma used for generating heatmaps
     normalize: bool = True
 ):
     keypoints = keypoints.detach().clone()
-    batch_dim = keypoints.shape[0]
-    n_keypoints = keypoints.shape[1]
     out_height = output_shape[0]
     out_width = output_shape[1]
     keypoints[:, :, 1] *= out_height / height
     keypoints[:, :, 0] *= out_width / width
-    confidence = torch.zeros((batch_dim, n_keypoints, out_height, out_width))
-    xv = torch.arange(out_width)
-    yv = torch.arange(out_height)
-    xx, yy = torch.meshgrid(xv, yv, indexing='xy')
+    nan_idxes = torch.isnan(keypoints)[:, :, 0]
+    #confidence = torch.zeros((batch_dim, n_keypoints, out_height, out_width), device=keypoints.device)
+    xv = torch.arange(out_width, device=keypoints.device)
+    yv = torch.arange(out_height, device=keypoints.device)
+    xx, yy = torch.meshgrid(yv, xv) #double check flipped order because of pytorch's ij and numpy's xy indexing for meshgrid 
     xx = xx.unsqueeze(0).unsqueeze(0)
-    #shape is now (1, 1, out_width, out_height)
     yy = yy.unsqueeze(0).unsqueeze(0)
-    #shape is now (1, 1, out_width, out_height)
-    confidence = (yy - keypoints[:, :, 0])
-    gaussian = yy keypoints[:, ]
-    for idx in range(n_keypoints):
-        keypoint = keypoints[idx]
-        if torch.any(keypoint != keypoint):  # keeps heatmaps with nans as all zeros
-            continue
-        gaussian = (yy - keypoint[1]) ** 2
-        gaussian += (xx - keypoint[0]) ** 2
-        gaussian *= -1
-        gaussian /= 2 * sigma ** 2
-        gaussian = torch.exp(gaussian)
-        confidence[..., idx] = gaussian
+    keypoints = keypoints.unsqueeze(2)
+    confidence = (yy - keypoints[:, :, :, :1]) ** 2
+    confidence += (xx - keypoints[:, :, :, 1:]) ** 2
+    confidence *= -1
+    confidence /= 2 * sigma ** 2
+    confidence = torch.exp(confidence)
     if not normalize:
-        confidence /= sigma * torch.sqrt(2 * torch.tensor(math.pi))
+        confidence /= sigma * torch.sqrt(2 * torch.tensor(math.pi), device = keypoints.device)
+    zero_heatmap = torch.zeros((out_height, out_width), device=keypoints.device)
+    confidence[nan_idxes] = zero_heatmap
     return confidence
+
--- tests/test_subpixmaxima.py
+++ tests/test_subpixmaxima.py
@@ -1,18 +1,14 @@
 from pose_est_nets.utils.heatmap_tracker_utils import SubPixelMaxima
 from pose_est_nets.datasets.datasets import HeatmapDataset
+from pose_est_nets.utils.dataset_utils import draw_keypoints, generate_heatmaps
 import imgaug.augmenters as iaa
 import torch
 from torch.utils.data import DataLoader
+import pytest
 
 _TORCH_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-
-def test_draw_keypoints():
-    
-
-
 def test_subpixmaxima():
-
     data_transform = []
     data_transform.append(
         iaa.Resize({"height": 384, "width": 384})
@@ -29,7 +25,7 @@ def test_subpixmaxima():
         output_sigma=torch.tensor(1.25, device=_TORCH_DEVICE),
         upsample_factor=torch.tensor(2, device=_TORCH_DEVICE),
         coordinate_scale=torch.tensor(4, device=_TORCH_DEVICE),  # 2 ** 2
-        confidence_scale=torch.tensor(1, device=_TORCH_DEVICE),  # was originally 255.0
+        confidence_scale=torch.tensor(1, device=_TORCH_DEVICE), 
         threshold=None,
         device=_TORCH_DEVICE
     )
@@ -53,13 +49,98 @@ def test_subpixmaxima():
     del img_batch, gt_keypts_batch
     torch.cuda.empty_cache()  # remove tensors from gpu
     
-    (maxima1, confidence1), (maxima2, confidence2) = SubPixMax.run(
-        gt_heatmap_batch.to(_TORCH_DEVICE),
-        gt_heatmap_batch.to(_TORCH_DEVICE)
+    maxima1, confidence1 = SubPixMax.run(
+        gt_heatmap_batch.to(_TORCH_DEVICE)   
     )
-    print(maxima1.shape, confidence1.shape, maxima2.shape, confidence2.shape)
+    print(maxima1.shape, confidence1.shape)
 
     # remove model/data from gpu; then cache can be cleared
     del gt_heatmap_batch
-    del maxima1, confidence1, maxima2, confidence2
+    del maxima1, confidence1
     torch.cuda.empty_cache()  # remove tensors from gpu
+
+
+def test_generate_keypoints():
+    data_transform = []
+    data_transform.append(
+        iaa.Resize({"height": 384, "width": 384})
+    )  # dlc dimensions need to be repeatably divisable by 2
+    imgaug_transform = iaa.Sequential(data_transform)
+    dataset = HeatmapDataset(
+        root_directory="toy_datasets/toymouseRunningData",
+        csv_path="CollectedData_.csv",
+        header_rows=[1, 2],
+        imgaug_transform=imgaug_transform,
+    )
+    test_img, gt_heatmap, gt_keypts = dataset.__getitem__(idx=0)
+    assert(gt_keypts.shape == (torch.Size([34])))
+    gt_heatmap = gt_heatmap.unsqueeze(0)
+    gt_keypts = gt_keypts.unsqueeze(0).reshape(1, 17, 2)
+    assert(gt_heatmap.shape == (1, 17, 96, 96))
+    torch_heatmap = generate_heatmaps(gt_keypts, height=384, width=384, output_shape=(96,96))
+    SubPixMax = SubPixelMaxima(
+        output_shape=(96, 96),
+        output_sigma=torch.tensor(1.25, device=_TORCH_DEVICE),
+        upsample_factor=torch.tensor(2, device=_TORCH_DEVICE),
+        coordinate_scale=torch.tensor(4, device=_TORCH_DEVICE),  # 2 ** 2
+        confidence_scale=torch.tensor(1, device=_TORCH_DEVICE), 
+        threshold=None,
+        device=_TORCH_DEVICE
+    )
+    og_maxima, gt_confidence = SubPixMax.run(gt_heatmap.to(_TORCH_DEVICE))
+    torch_maxima, confidence_t = SubPixMax.run(torch_heatmap.to(_TORCH_DEVICE))
+    print(og_maxima, torch_maxima, gt_keypts)
+    print(gt_confidence, confidence_t)
+    assert(og_maxima == torch_maxima).all()
+    assert(gt_confidence == confidence_t).all()
+
+def test_generate_keypoints_weird_shape():
+    data_transform = []
+    OG_SHAPE = (384, 256)
+    DOWNSAMPLE_FACTOR = 2
+    output_shape = (OG_SHAPE[0]//(2** DOWNSAMPLE_FACTOR), OG_SHAPE[1]//(2** DOWNSAMPLE_FACTOR))
+    data_transform.append(
+        iaa.Resize({"height": OG_SHAPE[0], "width": OG_SHAPE[1]})
+    )  # dlc dimensions need to be repeatably divisable by 2
+    imgaug_transform = iaa.Sequential(data_transform)
+    dataset = HeatmapDataset(
+        root_directory="toy_datasets/toymouseRunningData",
+        csv_path="CollectedData_.csv",
+        header_rows=[1, 2],
+        imgaug_transform=imgaug_transform,
+    )
+    test_img, gt_heatmap, gt_keypts = dataset.__getitem__(idx=0)
+    assert(gt_keypts.shape == (torch.Size([34])))
+    gt_heatmap = gt_heatmap.unsqueeze(0)
+    gt_keypts = gt_keypts.unsqueeze(0).reshape(1, 17, 2)
+    assert(gt_heatmap.shape == (1, 17, output_shape[0], output_shape[1]))
+    torch_heatmap = generate_heatmaps(gt_keypts, height=OG_SHAPE[0], width=OG_SHAPE[1], output_shape=output_shape)
+    SubPixMax = SubPixelMaxima(
+        output_shape=output_shape,
+        output_sigma=torch.tensor(1.25, device=_TORCH_DEVICE),
+        upsample_factor=torch.tensor(2, device=_TORCH_DEVICE),
+        coordinate_scale=torch.tensor(4, device=_TORCH_DEVICE),  # 2 ** 2
+        confidence_scale=torch.tensor(1, device=_TORCH_DEVICE), 
+        threshold=None,
+        device=_TORCH_DEVICE
+    )
+    og_maxima, gt_confidence = SubPixMax.run(gt_heatmap.to(_TORCH_DEVICE))
+    torch_maxima, confidence_t = SubPixMax.run(torch_heatmap.to(_TORCH_DEVICE))
+    print(og_maxima, torch_maxima, gt_keypts)
+    print(gt_confidence, confidence_t)
+    assert(og_maxima == torch_maxima).all()
+    assert(gt_confidence == confidence_t).all()
+
+def test_generate_keypoints_batched():
+    data_transform = []
+    data_transform.append(
+        iaa.Resize({"height": 384, "width": 384})
+    )  # dlc dimensions need to be repeatably divisable by 2
+    imgaug_transform = iaa.Sequential(data_transform)
+    dataset = HeatmapDataset(
+        root_directory="toy_datasets/toymouseRunningData",
+        csv_path="CollectedData_.csv",
+        header_rows=[1, 2],
+        imgaug_transform=imgaug_transform,
+    )
+    
