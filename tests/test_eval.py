ff8352338cb82e51c7c1786bbefe1c6df9160e81
danbider <>
2021-12-01 21:00:38 +0000

    changes after pairs prog with matt. most importantly, parameterize the penalty differently, as a function of the 1/2*weights^2, and just use those losses that the user specifies.

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -20,10 +20,12 @@ _WIDTH = 256
 
 data_transform = []
 data_transform.append(
-    iaa.Resize({
-        "height": cfg.data.image_resize_dims.height,
-        "width": cfg.data.image_resize_dims.width
-    })
+    iaa.Resize(
+        {
+            "height": cfg.data.image_resize_dims.height,
+            "width": cfg.data.image_resize_dims.width,
+        }
+    )
 )  # dlc dimensions need to be repeatably divisable by 2
 imgaug_transform = iaa.Sequential(data_transform)
 heatmap_data = HeatmapDataset(
@@ -36,46 +38,44 @@ heatmap_module = BaseDataModule(heatmap_data)  # and default args
 heatmap_module.setup()
 
 ckpt_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
-    monitor="val_loss", 
-    dirpath=hydra_output_directory,
-    save_last=True,
-    save_top_k=0
+    monitor="val_loss", dirpath=hydra_output_directory, save_last=True, save_top_k=0
 )
 
 trainer = pl.Trainer(
-        gpus=1 if _TORCH_DEVICE == "cuda" else 0,
-        max_epochs=2,
-        check_val_every_n_epoch=1,
-        log_every_n_steps=1,
-        callbacks=[ckpt_callback]
-)  
+    gpus=1 if _TORCH_DEVICE == "cuda" else 0,
+    max_epochs=2,
+    check_val_every_n_epoch=1,
+    log_every_n_steps=1,
+    callbacks=[ckpt_callback],
+)
 
 model = HeatmapTracker(num_targets=34)
 trainer.fit(model=model, datamodule=heatmap_module)
 model_ckpt = hydra_output_directory + "/last.ckpt"
 
+
 def test_predict_dataset_and_predict_video():
-    assert(os.path.exists(model_ckpt))
+    assert os.path.exists(model_ckpt)
     predict_dataset(
-        cfg=cfg, 
-        datamod=heatmap_module, 
-        hydra_output_directory=hydra_output_directory, 
-        ckpt_file=model_ckpt
+        cfg=cfg,
+        datamod=heatmap_module,
+        hydra_output_directory=hydra_output_directory,
+        ckpt_file=model_ckpt,
     )
-    assert(os.path.exists("toy_datasets/toy_model/predictions.csv"))
+    assert os.path.exists("toy_datasets/toy_model/predictions.csv")
     os.remove("toy_datasets/toy_model/predictions.csv")
-    assert(os.path.exists("toy_datasets/toy_model/dataset_split_indices.json"))
+    assert os.path.exists("toy_datasets/toy_model/dataset_split_indices.json")
     os.remove("toy_datasets/toy_model/dataset_split_indices.json")
 
     predict_videos(
         video_path="toy_datasets/toymouseRunningData/unlabeled_videos/",
         ckpt_file=model_ckpt,
         cfg_file=cfg,
-        save_file="toy_datasets/toy_model/video_predictions.csv"
+        save_file="toy_datasets/toy_model/video_predictions.csv",
     )
-    assert(os.path.exists("toy_datasets/toy_model/video_predictions.csv"))
+    assert os.path.exists("toy_datasets/toy_model/video_predictions.csv")
     os.remove("toy_datasets/toy_model/video_predictions.csv")
 
-    os.remove(model_ckpt)
-
-
+    os.remove(
+        "toy_datasets/toy_model/last.ckpt"
+    )  # this is not being removed for some reason
