fdddcd6779c0e75463c4177e75a590a7172d0186
themattinthehatt <themattinthehatt@gmail.com>
2022-01-10 00:49:23 -0500

    test refactoring: utils module

--- lightning_pose/utils/pca.py
+++ lightning_pose/utils/pca.py
@@ -1,11 +1,8 @@
-"""Preprocessing for specialized losses."""
+"""PCA class to assist with computing PCA losses."""
 
-import warnings
-from omegaconf import DictConfig, ListConfig
 import numpy as np
-from pytorch_lightning.core import datamodule
+from omegaconf import DictConfig, ListConfig
 from sklearn.decomposition import PCA
-import sklearn
 import torch
 from torchtyping import TensorType, patch_typeguard
 from typeguard import typechecked
@@ -13,7 +10,6 @@ from typing import List, Optional, Union, Literal, Dict
 import warnings
 
 from lightning_pose.data.datamodules import BaseDataModule, UnlabeledDataModule
-from lightning_pose.data.datasets import HeatmapDataset
 from lightning_pose.data.utils import clean_any_nans
 from lightning_pose.losses.helpers import (
     EmpiricalEpsilon,
@@ -30,6 +26,8 @@ patch_typeguard()  # use before @typechecked
 
 @typechecked
 class KeypointPCA(object):
+    """Class to collect data from a dataloader and compute PCA params."""
+
     def __init__(
         self,
         loss_type: Literal["pca_singleview", "pca_multiview"],
@@ -85,7 +83,7 @@ class KeypointPCA(object):
         # ensure we have more rows than columns after doing nan filtering
         if self.data_arr.shape[0] < self.data_arr.shape[1]:
             raise ValueError(
-                "cannot fit PCA with {} observations < {} observation dimensions".format(
+                "cannot fit PCA with {} samples < {} observation dimensions".format(
                     self.data_arr.shape[0], self.data_arr.shape[1]
                 )
             )
@@ -103,7 +101,9 @@ class KeypointPCA(object):
     def _choose_n_components(self) -> None:
         # TODO: should we return an integer and not override?
         if self.loss_type == "pca_multiview":
-            self._n_components_kept = 3  # all views can be explained by 3 (x,y,z) coords. ignore self.components_to_keep_argument
+            # all views can be explained by 3 (x,y,z) coords
+            # ignore self.components_to_keep_argument
+            self._n_components_kept = 3
             if self._n_components_kept != self.components_to_keep:
                 warnings.warn(
                     "for {} loss, you specified {} components_to_keep, but we will instead keep {} components".format(
@@ -116,7 +116,7 @@ class KeypointPCA(object):
                     fitted_pca_object=self.pca_object,
                     components_to_keep=self.components_to_keep,
                 ).__call__()
-        assert type(self._n_components_kept) is int
+        assert isinstance(self._n_components_kept, int)
 
     def pca_prints(self) -> None:
         # call after we've fitted a pca object and selected how many components to keep
@@ -146,32 +146,48 @@ class KeypointPCA(object):
         )
 
     def __call__(self):
-        # TODO: think if we always like to override data_arr. should we need a copy of it to undo the nan stuff?
-        self._get_data()  # save training data in self.data_arr, TODO: consider putting in init
-        self._format_data()  # modify self.data_arr in the case of multiview pca, else keep the same
-        self._clean_any_nans()  # remove those observations with more than one Nan. TODO: consider infilling somehow
-        self._check_data()  # check no nans, and that we have more observations than observation-dimensions
+
+        # TODO: think if we always like to override data_arr.
+        # should we need a copy of it to undo the nan stuff?
+
+        # save training data in self.data_arr
+        # TODO: consider putting in init
+        self._get_data()
+
+        # modify self.data_arr in the case of multiview pca, else keep the same
+        self._format_data()
+
+        # remove those observations with more than one Nan.
+        # TODO: consider infilling somehow
+        self._clean_any_nans()
+
+        # check no nans, and that we have more observations than observation-dimensions
+        self._check_data()
+
         self._fit_pca()
         self._choose_n_components()
         self.pca_prints()
-        self._set_parameter_dict()  # save all the meaningful quantities
-        # extract those relevant eigenvectors, make them into tensors, compute epsilon and return
-        # TODO: verify that the extracted params are fine, and that we can train with these.
+
+        # save all the meaningful quantities
+        self._set_parameter_dict()
 
 
 class ComponentChooser:
-    """determines the number of components to keep."""
+    """Determine the number of PCA components to keep."""
 
+    @typechecked
     def __init__(
         self,
-        fitted_pca_object: sklearn.decomposition.PCA,
+        fitted_pca_object: PCA,
         components_to_keep: Optional[Union[int, float]],
-    ):
+    ) -> None:
         self.fitted_pca_object = fitted_pca_object
-        self.components_to_keep = components_to_keep  # can be either a float indicating proportion of explained variance, or an integer specifying the number of components.
+        # can be either a float indicating proportion of explained variance, or an
+        # integer specifying the number of components
+        self.components_to_keep = components_to_keep
         self._check_components_to_keep()
 
-    # TODO: I dislike the confusing names (components_to_keep versus min_variance_explained)
+    # TODO: I dislike the confusing names (components_to_keep vs min_variance_explained)
 
     @property
     def cumsum_explained_variance(self):
@@ -199,11 +215,12 @@ class ComponentChooser:
         # find the index of the first element above a min_variance_explained threshold
         assert type(
             self.components_to_keep is float
-        )  # i.e., threshold crossing doesn't make sense with an integer components_to_keep
+        )  # i.e., threshold crossing doesn't make sense with integer components_to_keep
         components_to_keep = int(
             np.where(self.cumsum_explained_variance >= self.components_to_keep)[0][0]
         )
-        # cumsum is a d - 1 dimensional vector where the 0th element is the sum of the 0th and 1st element of the d dimensional vector it is summing over
+        # cumsum is a d - 1 dimensional vector where the 0th element is the sum of the
+        # 0th and 1st element of the d dimensional vector it is summing over
         return components_to_keep + 1
 
     def __call__(self) -> int:
@@ -215,29 +232,42 @@ class ComponentChooser:
             )  # find that integer that crosses the minimum explained variance
 
 
-# TODO: add TensorType
 @typechecked
 def compute_pca_reprojection_error(
-    clean_pca_arr: torch.Tensor,
-    kept_eigenvectors: torch.Tensor,
-    mean: torch.Tensor,
-) -> torch.Tensor:
+    clean_pca_arr: TensorType["samples", "observation_dim"],
+    kept_eigenvectors: TensorType["latent_dim", "observation_dim"],
+    mean: TensorType["observation_dim"],
+) -> TensorType["samples", "observation_dim_div_by_two"]:
+
     # first verify that the pca array has observations divisible by 2
     # (corresponding to (x,y) coords)
-    clean_pca_arr = clean_pca_arr - mean.unsqueeze(0)  # mean-center
     assert clean_pca_arr.shape[1] % 2 == 0
+
+    # mean-center
+    clean_pca_arr = clean_pca_arr - mean.unsqueeze(0)
+
+    # project down into low-d space, then back up to observation space
     reprojection_arr = (
         clean_pca_arr @ kept_eigenvectors.T @ kept_eigenvectors
     )  # e.g., (214, 4) X (4, 3) X (3, 4) = (214, 4) as we started
-    diff = (
-        clean_pca_arr - reprojection_arr
-    )  # shape: (num_samples: for multiview (num_samples=num_samples X num_bodyparts), observation_dim: for multiview (2 X num_views), for singleview (2 X num_bodyparts))
-    # reshape:
+
+    # compute difference between original array and its reprojection
+    # multiview:
+    # - num_samples: num_samples X num_bodyparts
+    # - observation_dim: 2 x num_views
+    # singleview:
+    # - num_samples: num_samples
+    # - observation_dim: 2 X num_bodyparts
+    diff = clean_pca_arr - reprojection_arr
+
+    # reshape to get a per sample/keypoint values:
     diff_arr_per_keypoint = diff.reshape(diff.shape[0], diff.shape[1] // 2, 2)
     reprojection_loss = torch.linalg.norm(diff_arr_per_keypoint, dim=2)
+
     # print("reprojection_loss.shape: {}".format(reprojection_loss.shape))
     # print("diff: {}".format(diff[:5, :]))
     # print("diff_arr_per_keypoint: {}".format(diff_arr_per_keypoint[:5, :, :]))
+
     return reprojection_loss
 
 
@@ -279,9 +309,9 @@ def pca_prints(pca: PCA, condition: str, components_to_keep: int) -> None:
 @typechecked
 def format_multiview_data_for_pca(
     data_arr: TensorType["batch", "num_keypoints", "2"],
-    mirrored_column_matches: Union[ListConfig, List],
+    mirrored_column_matches: Union[ListConfig, list],
 ) -> TensorType["batch_times_num_keypoints", "two_times_num_views"]:
-    """
+    """Reformat multiview data so each observation is a single body part across views.
 
     Args:
         data_arr: keypoints from training data
