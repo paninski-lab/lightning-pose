61777d099cd6eb1d02b7975a6a12908554eec114
themattinthehatt <themattinthehatt@gmail.com>
2022-02-10 15:05:43 -0500

    fix fiftyone test
    Former-commit-id: 38e1eb3ee45d777f973b2eedb3d042323a583a8d

--- tests/utils/test_df_ops.py
+++ tests/utils/test_df_ops.py
@@ -1,11 +1,13 @@
+
 def test_dfConverter():
+
     import pandas as pd
+    from lightning_pose.utils.fiftyone import dfConverter
 
     df = pd.read_csv(
         "toy_datasets/toymouseRunningData/CollectedData_.csv", header=[1, 2]
     )
-    from lightning_pose.utils.fiftyone import dfConverter
+    keypoint_names = [c[0] for c in df.columns[1::2]]
 
-    out = dfConverter(df)()
+    out = dfConverter(df, keypoint_names=keypoint_names)()
     assert type(out) is dict
-    # print(df.head())
