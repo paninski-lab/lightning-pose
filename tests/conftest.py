7391c67a7b17408726ab26ecbe1587c0e8565e53
danbider <>
2022-06-13 14:14:43 +0000

    reading the dali config file

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ import yaml
 
 from lightning_pose.data.datamodules import BaseDataModule, UnlabeledDataModule
 from lightning_pose.data.datasets import BaseTrackingDataset, HeatmapDataset
-from lightning_pose.utils.predictions import get_videos_in_dir
+from lightning_pose.utils.io import get_videos_in_dir
 from lightning_pose.utils.scripts import (
     get_data_module,
     get_dataset,
@@ -37,7 +37,7 @@ def cfg() -> dict:
     base_dir = os.path.dirname(os.path.dirname(os.path.join(__file__)))
     config_dir = os.path.join(base_dir, "scripts", "configs")
 
-    keys = ["data", "losses", "model", "training"]
+    keys = ["data", "losses", "model", "training", "dali"]
     cfg = {}
 
     for key in keys:
