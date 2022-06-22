b17f3be74d0fe22edb30657a3e229113541be0a9
themattinthehatt <themattinthehatt@gmail.com>
2022-06-22 15:48:48 -0400

    get rid of unused args

--- lightning_pose/utils/scripts.py
+++ lightning_pose/utils/scripts.py
@@ -263,7 +263,6 @@ def get_data_module(
             val_probability=cfg.training.val_prob,
             train_frames=cfg.training.train_frames,
             dali_config=cfg.dali,
-            unlabeled_sequence_length=cfg.training.unlabeled_sequence_length,
             torch_seed=cfg.training.rng_seed_data_pt,
             dali_seed=cfg.training.rng_seed_data_dali,
             device_id=cfg.training.gpu_id,
