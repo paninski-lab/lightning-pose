c46f5b80a8e26ee2e9c5d496525dfebaf8adaea8
danbider <danbider@gmail.com>
2022-07-13 10:50:02 -0400

    all the command line args will be called from outside, appending config-path and config-name per dataset inside
    Former-commit-id: 9a61db22d8c2bc21679aa46ae87429dfbff95f9e

--- scripts/grid-run-hydra-1.sh
+++ scripts/grid-run-hydra-1.sh
@@ -1,6 +1,5 @@
+# hydra args could be modified here. if use backslash, keep a space below it.
 python scripts/train_hydra.py \
 --multirun \
-training.train_batch_size=8 \
-training.train_frames=125 \
-model.model_name="singleview_sweep" \
-dali.base.train.sequence_length=32 \
+--config-path=scripts/configs_mirror-mouse \
+--config-name=config_mirror-mouse \
