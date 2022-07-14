c7f7093ee33fc17d7efe1e8155ce8a8182d44b0f
Robert S Lee <sangkyulee@gmail.com>
2022-07-14 14:34:54 -0400

    more comments and Dockerfile required to run the real jobs

--- grid-hpo.sh
+++ grid-hpo.sh
@@ -1,29 +1,24 @@
 #!/usr/bin/env bash
 
-# test locally
-# scripts/grid-hpo.sh --script scripts/grid-run-test-1.sh --training.rng_seed_data_pt "[1,2]" --dali.base.train.sequence_length "[4,5]"
-# scripts/grid-hpo.sh --script scripts/grid-run-test-2.sh --training.rng_seed_data_pt "[1,2]" --dali.base.train.sequence_length "[4,5]"
-# scripts/grid-hpo.sh --script scripts/grid-run-hydra-1.sh --training.rng_seed_data_pt "[1,2]" --dali.base.train.sequence_length "[4,5]"
-
-# real run
-# grid run --localdir scripts/grid-hpo.sh --script scripts/grid-run-test-1.sh --training.rng_seed_data_pt "[1,2]" --dali.base.train.sequence_length "[4,5]"
-# grid run --localdir scripts/grid-hpo.sh --script scripts/grid-run-test-2.sh --training.rng_seed_data_pt "[1,2]" --dali.base.train.sequence_length "[4,5]"
-# grid run --localdir scripts/grid-run-hydra-1.sh --script scripts/grid-run-test.sh --training.rng_seed_data_pt "[1,2]" --dali.base.train.sequence_length "[4,5]"
-
+# save --config-path or --config-dir or --config-name
 config_arg=""
+# save the rest of the hydra params abc=123 format
 param_arg=""
+# the proxy script that will run with ${config_args} and ${param_args}
 touch test.$$.sh
 chmod a+x test.$$.sh
 
 # args must follow  follow --key value format
-# $0 is the script name, $1 is the first arg, $2 is the second arg, etc. in our case $1 is --key, $2 is value etc.
+# $0 is the script name, $1 is the first arg, $2 is the second arg, etc.
+# in our case $1 is --key, $2 is value etc.
 # we have $I where I is the total number of args passed to the script. $# is the number of args.
 while true; do
-  # assume --key and remove the leading --
+  # key
 	key="${1}"
+  # if key is empty, then break out of the loop
   if [ -z "${key}" ]; then break; fi
   value=${2}
-  # parse script 
+  # parse script that will be run as the proxy
   if [ "${key}" == "--script" ]; then
     if [ ! -f ${value} ]; then
       echo "script ${value} is not found"
