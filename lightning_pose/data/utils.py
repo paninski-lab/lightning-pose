899341b8c2f42202457fc431488028789323befa
colehurwitz <colehurwitz@gmail.com>
2022-07-25 21:39:38 -0400

    updated confidence and tests

--- lightning_pose/data/utils.py
+++ lightning_pose/data/utils.py
@@ -306,21 +306,22 @@ def evaluate_heatmaps_at_location(
     sigma: Union[float, int] = 1.25,  # sigma used for generating heatmaps
 ) -> TensorType["batch", "num_keypoints"]:
     """Evaluate 4D heatmaps using a 3D location tensor (last dim is x, y coords)."""
-    i = torch.arange(heatmaps.shape[0]).reshape(-1, 1, 1, 1)
-    j = torch.arange(heatmaps.shape[1]).reshape(1, -1, 1, 1)
-    k = locs[:, :, None, 1, None].type(torch.int64)
-    l = locs[:, :, 0, None, None].type(torch.int64)
+    num_pad = 3
+    heatmaps_padded = torch.zeros(heatmaps.shape[0], heatmaps.shape[1], heatmaps.shape[2] + num_pad*2, heatmaps.shape[3] + num_pad*2)
+    heatmaps_padded[:,:, num_pad:-num_pad, num_pad:-num_pad] = heatmaps
+    i = torch.arange(heatmaps_padded.shape[0]).reshape(-1, 1, 1, 1)
+    j = torch.arange(heatmaps_padded.shape[1]).reshape(1, -1, 1, 1)
+    k = locs[:, :, None, 1, None].type(torch.int64) + num_pad
+    l = locs[:, :, 0, None, None].type(torch.int64) + num_pad
     pix_to_consider = int(np.ceil(sigma * 2.0))  # get all pixels within two stds.
     offsets = list(np.arange(-pix_to_consider, pix_to_consider + 1))
     vals_all = []
     for offset in offsets:
         k_offset = k + offset
-        k_offset = torch.clamp(k_offset, min=0, max=heatmaps.shape[2] - 1)
         for offset_2 in offsets:
             l_offset = l + offset_2
-            l_offset = torch.clamp(l_offset, min=0, max=heatmaps.shape[3] - 1)
             vals = (
-                heatmaps[i, j, k_offset, l_offset].squeeze(-1).squeeze(-1)
+                heatmaps_padded[i, j, k_offset, l_offset].squeeze(-1).squeeze(-1)
             )  # get rid of singleton dims
             vals_all.append(vals)
     vals = torch.stack(vals_all, 0).sum(0)
