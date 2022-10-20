d2ca25ef49da9362a0f89c17b1ac04b80fd5e7f9
themattinthehatt <themattinthehatt@gmail.com>
2022-10-20 14:47:08 -0400

    add semi-super mhcrnn

--- lightning_pose/models/heatmap_tracker.py
+++ lightning_pose/models/heatmap_tracker.py
@@ -291,7 +291,8 @@ class SemiSupervisedHeatmapTracker(SemiSupervisedTrackerMixin, HeatmapTracker):
         backbone: Literal[
             "resnet18", "resnet34", "resnet50", "resnet101", "resnet152",
             "resnet50_3d", "resnet50_contrastive", "resnet50_animal_apose", "resnet50_animal_ap10k", 
-            "resnet50_human_jhmdb", "resnet50_human_res_rle", "resnet50_human_top_res"] = "resnet50",
+            "resnet50_human_jhmdb", "resnet50_human_res_rle", "resnet50_human_top_res"
+        ] = "resnet50",
         downsample_factor: Literal[2, 3] = 2,
         pretrained: bool = True,
         last_resnet_layer_to_get: int = -3,
--- tests/models/test_heatmap_tracker_mhcrnn.py
+++ tests/models/test_heatmap_tracker_mhcrnn.py
@@ -1,4 +1,4 @@
-"""Test the initialization and training of heatmap multi-head crnn models."""
+"""Test the initialization and training of context heatmap multi-head crnn models."""
 
 import copy
 import pytest
@@ -7,7 +7,7 @@ from lightning_pose.utils.tests import run_model_test
 
 
 def test_supervised_heatmap_mhcrnn(
-        cfg_context, heatmap_data_module_context, video_dataloader, trainer, remove_logs
+    cfg_context, heatmap_data_module_context, video_dataloader, trainer, remove_logs
 ):
     """Test the initialization and training of a supervised heatmap mhcrnn model."""
 
@@ -24,41 +24,23 @@ def test_supervised_heatmap_mhcrnn(
     )
 
 
-# def test_semisupervised_heatmap_temporal(
-#     cfg, heatmap_data_module_combined, video_dataloader, trainer, remove_logs,
-# ):
-#     """Test the initialization and training of an semi-supervised heatmap model."""
-#
-#     cfg_tmp = copy.deepcopy(cfg)
-#     cfg_tmp.model.model_type = "heatmap"
-#     cfg_tmp.model.losses_to_use = ["temporal"]
-#
-#     run_model_test(
-#         cfg=cfg_tmp,
-#         data_module=heatmap_data_module_combined,
-#         video_dataloader=video_dataloader,
-#         trainer=trainer,
-#         remove_logs_fn=remove_logs,
-#     )
-#
-#
-# def test_semisupervised_heatmap_pcasingleview_context(
-#     cfg_context, heatmap_data_module_combined_context, video_dataloader, trainer, remove_logs,
-# ):
-#     """Test the initialization and training of an semi-supervised heatmap context model.
-#
-#     NOTE: the toy dataset is not a proper context dataset
-#
-#     """
-#
-#     cfg_tmp = copy.deepcopy(cfg_context)
-#     cfg_tmp.model.model_type = "heatmap"
-#     cfg_tmp.model.losses_to_use = ["pca_singleview"]
-#
-#     run_model_test(
-#         cfg=cfg_tmp,
-#         data_module=heatmap_data_module_combined_context,
-#         video_dataloader=video_dataloader,
-#         trainer=trainer,
-#         remove_logs_fn=remove_logs,
-#     )
+def test_semisupervised_heatmap_mhcrnn_pcasingleview(
+    cfg_context, heatmap_data_module_combined_context, video_dataloader, trainer, remove_logs,
+):
+    """Test the initialization and training of an semi-supervised heatmap mhcrnn model.
+
+    NOTE: the toy dataset is not a proper context dataset
+
+    """
+
+    cfg_tmp = copy.deepcopy(cfg_context)
+    cfg_tmp.model.model_type = "heatmap_mhcrnn"
+    cfg_tmp.model.losses_to_use = ["pca_singleview"]
+
+    run_model_test(
+        cfg=cfg_tmp,
+        data_module=heatmap_data_module_combined_context,
+        video_dataloader=video_dataloader,
+        trainer=trainer,
+        remove_logs_fn=remove_logs,
+    )
