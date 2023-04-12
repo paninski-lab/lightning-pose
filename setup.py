565ae136f9bc976f4362305a08b515f99357a545
danbider <danbider@gmail.com>
2023-04-12 14:00:36 -0400

    adding a long description from readme

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages, setup
 
 VERSION = "0.0.1"  # was previously None
 
+# add the README.md file to the long_description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 install_requires = [
     "black",
     "fiftyone",
@@ -37,7 +41,9 @@ setup(
     packages=find_packages(),
     version=VERSION,
     description="Semi-supervised pose estimation using Lightning.",
-    author="Dan Biderman",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Dan Biderman and Matt Whiteway",
     install_requires=install_requires,  # load_requirements(PATH_ROOT),
     author_email="danbider@gmail.com",
     url="https://github.com/danbider/lightning-pose",
