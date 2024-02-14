094747b22fbf3f2acc5d1b36b8f25778825bff7f
themattinthehatt <themattinthehatt@gmail.com>
2024-02-14 15:14:32 -0500

    fiftyone: remove unused video plotting functionality

--- lightning_pose/utils/fiftyone.py
+++ lightning_pose/utils/fiftyone.py
@@ -15,7 +15,6 @@ from lightning_pose.utils.io import return_absolute_data_paths, return_absolute_
 __all__ = [
     "check_lists_equal",
     "remove_string_w_substring_from_list",
-    "check_unique_tags",
     "check_dataset",
     "get_image_tags",
     "FiftyOneKeypointBase",
@@ -40,17 +39,6 @@ def remove_string_w_substring_from_list(strings: List[str], substring: str) -> L
 
 
 @typechecked
-def check_unique_tags(data_pt_tags: List[str]) -> bool:
-    uniques = list(np.unique(data_pt_tags))
-    cond_list = ["test", "train", "validation"]
-    cond_list_with_unused_images = ["test", "train", "validation", "unused"]
-    flag = check_lists_equal(uniques, cond_list) or check_lists_equal(
-        uniques, cond_list_with_unused_images
-    )
-    return flag
-
-
-@typechecked
 def check_dataset(dataset: fo.Dataset) -> None:
     pretty_print_str("Checking FiftyOne.Dataset by computing metadata... ")
     try:
@@ -58,9 +46,7 @@ def check_dataset(dataset: fo.Dataset) -> None:
     except ValueError:
         print("Encountered error in metadata computation. See print:")
         print(dataset.exists("metadata", False))
-        print(
-            "The above print should indicate bad image samples, e.g., with bad paths."
-        )
+        print("The above print should indicate bad image samples, e.g., with bad paths.")
 
 
 @typechecked
@@ -76,12 +62,13 @@ def get_image_tags(pred_df: pd.DataFrame) -> pd.Series:
 # #@typechecked
 # force typechecking over the entire class. right now fails due to some
 # list/listconfig issue
-class FiftyOneKeypointBase:
+class FiftyOneImagePlotter:
 
     def __init__(
         self,
         cfg: DictConfig,
         keypoints_to_plot: Optional[List[str]] = None,
+        csv_filename: str = "predictions.csv",
     ) -> None:
 
         self.cfg = cfg
@@ -98,9 +85,7 @@ class FiftyOneKeypointBase:
         )
         if self.keypoints_to_plot is None:
             # plot all keypoints that appear in the ground-truth dataframe
-            self.keypoints_to_plot: List[str] = list(
-                self.ground_truth_df.columns.levels[0]
-            )
+            self.keypoints_to_plot: List[str] = list(self.ground_truth_df.columns.levels[0])
             # remove "bodyparts"
             if "bodyparts" in self.keypoints_to_plot:
                 self.keypoints_to_plot.remove("bodyparts")
@@ -115,9 +100,29 @@ class FiftyOneKeypointBase:
         self.gt_data_dict: Dict[str, Dict[str, np.array]] = dfConverter(
             df=self.ground_truth_df, keypoint_names=self.keypoints_to_plot
         )()
-        # self.model_abs_paths = self.get_model_abs_paths()
-        #
-        self.pred_csv_files = []  # override in subclasses
+
+        model_abs_paths = self.get_model_abs_paths()
+        self.pred_csv_files = [
+            os.path.join(model_dir, csv_filename) for model_dir in model_abs_paths
+        ]
+
+    @property
+    def image_paths(self) -> List[str]:
+        """extract absolute paths for all the images in the ground truth csv file
+
+        Returns:
+            List[str]: absolute paths per image, checked before returning.
+        """
+        relative_list = list(self.ground_truth_df.iloc[:, 0])
+        absolute_list = [
+            os.path.join(self.data_dir, im_path) for im_path in relative_list
+        ]
+        # assert that the images are indeed files
+        for im in absolute_list:
+            if not os.path.isfile(im):
+                raise FileNotFoundError(im)
+
+        return absolute_list
 
     @property
     def img_width(self) -> int:
@@ -157,6 +162,11 @@ class FiftyOneKeypointBase:
             assert os.path.isdir(mod_path)
         return model_abs_paths
 
+    def get_gt_keypoints_list(self) -> List[fo.Keypoints]:
+        # for each frame, extract ground-truth keypoint information
+        print("Collecting ground-truth keypoints...")
+        return self.get_keypoints_per_image(self.gt_data_dict)
+
     def load_model_predictions(self) -> None:
         # take the abs paths, and load the models into a dictionary
         self.model_preds_dict = {}
@@ -165,12 +175,9 @@ class FiftyOneKeypointBase:
             # assuming that each path of saved logs has a predictions.csv file in it
             # always assume [1, 2] since our code generated the predictions
             temp_df = pd.read_csv(pred_csv_file, header=[1, 2])
-            self.model_preds_dict[model_name] = dfConverter(
-                temp_df, self.keypoints_to_plot
-            )()
+            self.model_preds_dict[model_name] = dfConverter(temp_df, self.keypoints_to_plot)()
             self.preds_pandas_df_dict[model_name] = temp_df
 
-    # @typechecked
     def build_single_frame_keypoints(
         self,
         data_dict: Dict[str, Dict[str, np.array]],
@@ -194,7 +201,6 @@ class FiftyOneKeypointBase:
             )
         return keypoints_list
 
-    # @typechecked
     def get_keypoints_per_image(
         self, data_dict: Dict[str, Dict[str, np.array]]
     ) -> List[fo.Keypoints]:
@@ -208,7 +214,6 @@ class FiftyOneKeypointBase:
             keypoints_list.append(fo.Keypoints(keypoints=single_frame_keypoints_list))
         return keypoints_list
 
-    # @typechecked
     def get_pred_keypoints_dict(self) -> Dict[str, List[fo.Keypoints]]:
         pred_keypoints_dict = {}
         # loop over the dictionary with predictions per model
@@ -217,50 +222,6 @@ class FiftyOneKeypointBase:
             pred_keypoints_dict[model_name] = self.get_keypoints_per_image(model_dict)
 
         return pred_keypoints_dict
-
-    def create_dataset(self):
-        # subclasses build their own
-        raise NotImplementedError
-
-
-# @typechecked
-class FiftyOneImagePlotter(FiftyOneKeypointBase):
-
-    def __init__(
-        self,
-        cfg: DictConfig,
-        keypoints_to_plot: Optional[List[str]] = None,
-        csv_filename: str = "predictions.csv",
-    ) -> None:
-        super().__init__(cfg=cfg, keypoints_to_plot=keypoints_to_plot)
-
-        model_abs_paths = self.get_model_abs_paths()
-        self.pred_csv_files = [
-            os.path.join(model_dir, csv_filename) for model_dir in model_abs_paths
-        ]
-
-    @property
-    def image_paths(self) -> List[str]:
-        """extract absolute paths for all the images in the ground truth csv file
-
-        Returns:
-            List[str]: absolute paths per image, checked before returning.
-        """
-        relative_list = list(self.ground_truth_df.iloc[:, 0])
-        absolute_list = [
-            os.path.join(self.data_dir, im_path) for im_path in relative_list
-        ]
-        # assert that the images are indeed files
-        for im in absolute_list:
-            if not os.path.isfile(im):
-                raise FileNotFoundError(im)
-
-        return absolute_list
-
-    def get_gt_keypoints_list(self) -> List[fo.Keypoints]:
-        # for each frame, extract ground-truth keypoint information
-        print("Collecting ground-truth keypoints...")
-        return self.get_keypoints_per_image(self.gt_data_dict)
 
     def create_dataset(self) -> fo.Dataset:
         samples = []
@@ -273,9 +234,7 @@ class FiftyOneImagePlotter(FiftyOneKeypointBase):
         gt_keypoints_list = self.get_gt_keypoints_list()
         # do the same for each model's predictions (lists are stored in a dict)
         pred_keypoints_dict = self.get_pred_keypoints_dict()
-        pretty_print_str(
-            "Appending fiftyone.Keypoints to fiftyone.Sample objects, for each image..."
-        )
+        pretty_print_str("Appending fo.Keypoints to fo.Sample objects for each image...")
         for img_idx, img_path in enumerate(tqdm(self.image_paths)):
             # create a "sample" with an image and a tag (should be appended to self.samples)
             sample = fo.Sample(filepath=img_path, tags=[self.data_tags[img_idx]])
@@ -292,57 +251,6 @@ class FiftyOneImagePlotter(FiftyOneKeypointBase):
         fiftyone_dataset.add_samples(samples)
         pretty_print_str("Done!")
         return fiftyone_dataset
-
-
-# @typechecked
-class FiftyOneKeypointVideoPlotter(FiftyOneKeypointBase):
-
-    def __init__(
-        self,
-        cfg: DictConfig,
-        keypoints_to_plot: Optional[List[str]] = None,
-        **kwargs  # make robust to other inputs
-    ) -> None:
-        # initialize FiftyOneKeypointBase
-        super().__init__(cfg=cfg, keypoints_to_plot=keypoints_to_plot)
-
-        self.video: str = cfg.eval.video_file_to_plot
-        self.pred_csv_files: List[str] = self.cfg.eval.pred_csv_files_to_plot
-        # self.pred_csv_files overrides the attribute in FiftyOneKeypointBase
-        self.check_inputs()
-        self.dataset_name = self.dataset_name + "_video"
-
-    def check_inputs(self) -> None:
-        for f in self.pred_csv_files:
-            if not os.path.isfile(f):
-                raise FileNotFoundError(f)
-        if not os.path.isfile(self.video):
-            raise FileNotFoundError(self.video)
-
-    def create_dataset(self) -> fo.Dataset:
-        # read each model's csv into a pandas dataframe, save in self.model_preds_dict
-        self.load_model_predictions()
-        # modify the predictions into fiftyone format
-        pred_keypoints_dict = self.get_pred_keypoints_dict()
-        # inherited from FiftyOneKeypointBase
-        dataset = fo.Dataset(self.dataset_name, persistent=True)
-        # adding _videos so as to not overwrite existing datasets with images.
-        # NOTE: for now, one sample only in the dataset (one video)
-        video_sample = fo.Sample(filepath=self.video)
-        first_model_name = list(pred_keypoints_dict.keys())[0]
-        pretty_print_str(
-            "Appending fiftyone.Keypoints to a fiftyone.Sample object for video, for each frame..."
-        )
-        for frame_idx in tqdm(range(len(pred_keypoints_dict[first_model_name]))):
-            for model_field_name, model_preds in pred_keypoints_dict.items():
-                video_sample.frames[frame_idx + 1][
-                    model_field_name + "_preds"
-                ] = model_preds[frame_idx]
-                # fo.Frame(keypoints=model_preds[frame_idx]) raised some issues
-        pretty_print_str("Adding a fiftyone.Sample to fiftyone.Dataset...")
-        dataset.add_sample(video_sample)
-        pretty_print_str("Done!")
-        return dataset
 
 
 # @typechecked
@@ -368,16 +276,3 @@ class dfConverter:
             full_dict[kp_name] = self.dict_per_bp(kp_name)
 
         return full_dict
-
-
-# @typechecked
-class FiftyOneFactory:
-
-    def __init__(self, dataset_to_create: Literal["images", "videos"]) -> None:
-        self.dataset_to_create = dataset_to_create
-
-    def __call__(self) -> Union[FiftyOneImagePlotter, FiftyOneKeypointVideoPlotter]:
-        if self.dataset_to_create == "images":
-            return FiftyOneImagePlotter
-        else:
-            return FiftyOneKeypointVideoPlotter
---
+++ tests/utils/test_fiftyone.py
@@ -0,0 +1,39 @@
+"""Test the FiftyOne module."""
+
+# import numpy as np
+import os
+import pandas as pd
+import shutil
+# import pytest
+
+
+def test_fiftyone_image_plotter(cfg, tmpdir):
+
+    from lightning_pose.utils.fiftyone import FiftyOneImagePlotter
+
+    # copy ground truth labels to a "predictions" file
+    data_dir_abs = os.path.abspath(cfg.data.data_dir)
+    gt_file = os.path.join(data_dir_abs, cfg.data.csv_file)
+    model_dir = os.path.join(tmpdir, "date", "time")
+    os.makedirs(model_dir, exist_ok=True)
+    pred_file = os.path.join(model_dir, "predictions.csv")
+    shutil.copyfile(gt_file, pred_file)
+
+    # add final column to "predictions"
+    df = pd.read_csv(pred_file, header=[0, 1, 2], index_col=0)
+    df.loc[:, ("set", "", "")] = "train"
+    df.to_csv(pred_file)
+    n_preds = df.shape[0]
+
+    # update config
+    cfg_new = cfg.copy()
+    cfg_new.data.data_dir = data_dir_abs
+    cfg_new.eval.model_display_names = ["test_model"]
+    cfg_new.eval.hydra_paths = [model_dir]
+    cfg_new.eval.fiftyone.dataset_name = str(tmpdir)  # get unique dataset name
+
+    # make fiftyone dataset and check
+    plotter = FiftyOneImagePlotter(cfg=cfg_new)
+    dataset = plotter.create_dataset()
+
+    assert len(dataset) == n_preds
