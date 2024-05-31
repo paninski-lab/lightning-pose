686b6c0d75411527de9c83a4c33604c3f5a7e233
themattinthehatt <themattinthehatt@gmail.com>
2024-05-31 12:13:45 -0400

    misc updates to docs and tests

--- tests/utils/test_fiftyone.py
+++ tests/utils/test_fiftyone.py
@@ -3,6 +3,7 @@ import os
 import shutil
 from datetime import datetime
 
+import numpy as np
 import pandas as pd
 
 
@@ -25,12 +26,39 @@ def test_fiftyone_image_plotter(cfg, tmpdir):
     df.to_csv(pred_file)
     n_preds = df.shape[0]
 
+    # -----------------------
+    # test 1: standard setup
+    # -----------------------
     # update config
     cfg_new = cfg.copy()
     cfg_new.data.data_dir = data_dir_abs
     cfg_new.eval.model_display_names = ["test_model"]
     cfg_new.eval.hydra_paths = [model_dir]
     cfg_new.eval.fiftyone.dataset_name = str(model_dir)  # get unique dataset name
+
+    # make fiftyone dataset and check
+    plotter = FiftyOneImagePlotter(cfg=cfg_new)
+    dataset = plotter.create_dataset()
+
+    assert len(dataset) == n_preds
+
+    # -----------------------
+    # test 2: missing labels
+    # -----------------------
+    # load ground truth file
+    gt_df = pd.read_csv(gt_file, header=[0, 1, 2], index_col=0)
+    # set all ground truth labels to nan
+    gt_df.iloc[:, :] = np.nan
+    csv_file = os.path.join(tmpdir, "CollectedData_all_nans")
+    gt_df.to_csv(csv_file)
+
+    # update config
+    cfg_new = cfg.copy()
+    cfg_new.data.data_dir = data_dir_abs
+    cfg_new.data.csv_file = csv_file
+    cfg_new.eval.model_display_names = ["test_model"]
+    cfg_new.eval.hydra_paths = [model_dir]
+    cfg_new.eval.fiftyone.dataset_name = str(model_dir) + "1"  # get unique dataset name
 
     # make fiftyone dataset and check
     plotter = FiftyOneImagePlotter(cfg=cfg_new)
