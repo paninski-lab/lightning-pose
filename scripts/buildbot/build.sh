cf0933b662a310625e172b35a919c1c594de2688
Karan Sikka <karanssikka@gmail.com>
2025-02-23 08:10:26 -0500

    Update script

--- scripts/buildbot/build.sh
+++ scripts/buildbot/build.sh
@@ -4,8 +4,8 @@ set -e
 USER=paninski-lab
 REPO_NAME=lightning-pose
 
-BASE_DIR=/local/`whoami`/builds
-TARGET_DIR=$BASE_DIR/`date '+%Y_%m_%d-%H_%M_%S'`
+BASE_DIR=/local/$(whoami)/builds
+TARGET_DIR=$BASE_DIR/$(date '+%Y_%m_%d-%H_%M_%S')
 CONDA_ENV=lp
 
 # Check if the PR number argument is provided
@@ -20,13 +20,21 @@ PR_NUMBER="$1"
 
 ml Miniforge-24.7.1-2
 conda activate $CONDA_ENV
-rm -rf $TARGET_DIR
+
+# Remove builds older than 24 hours
+find "$BASE_DIR" -maxdepth 1 -type d -mtime +0 -print0 | while IFS= read -r -d $'\0' directory; do
+  # Skip the starting directory itself.
+  if [[ "$directory" != "$BASE_DIR" ]]; then
+      echo "Removing directory: $directory"
+      rm -rf "$directory"
+  fi
+done
 
 # Get the code of the PR.
 # For efficiency, rather than cloning, it inits a blank repo
 # and fetches just the code we need.
-git init $TARGET_DIR
-cd $TARGET_DIR
+git init "$TARGET_DIR"
+cd "$TARGET_DIR"
 git remote add upstream "https://github.com/$USER/$REPO_NAME.git"
 git fetch upstream "refs/pull/$PR_NUMBER/merge"
 git checkout FETCH_HEAD
