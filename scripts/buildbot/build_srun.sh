05886668287ac688cf26308ecde88dac391437c2
Karan Sikka <karanssikka@gmail.com>
2025-02-24 15:27:57 -0500

    CI: fix workflow cancellations

--- scripts/buildbot/build_srun.sh
+++ scripts/buildbot/build_srun.sh
@@ -1,1 +1,5 @@
-srun --unbuffered -t 1:00:00 -c4 --mem=8g --gres=gpu:1 buildbot/build.sh $@
+# About the flags:
+#   -X is needed for github cancel to work.
+#     Otherwise srun needs two successive ctrl-C / SIGINT to cancel
+#   -u was found to enable colors. --pty would also work.
+srun -X -u -t 1:00:00 -c4 --mem=8g --gres=gpu:1 buildbot/build.sh $@
