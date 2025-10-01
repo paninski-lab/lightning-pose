99eb8d86556c7a4050141938825cd0a57bfc1fb9
themattinthehatt <themattinthehatt@gmail.com>
2025-10-01 11:20:45 -0400

    remove unused multiview experimental code

--- lightning_pose/models/heads/__init__.py
+++ lightning_pose/models/heads/__init__.py
@@ -2,20 +2,8 @@ from typing import Union
 
 from lightning_pose.models.heads.heatmap import HeatmapHead
 from lightning_pose.models.heads.heatmap_mhcrnn import HeatmapMHCRNNHead
-from lightning_pose.models.heads.heatmap_multiview import (
-    MultiviewHeatmapCNNHead,
-    MultiviewHeatmapCNNMultiHead,
-)
 
 from lightning_pose.models.heads.regression import LinearRegressionHead
-
-ALLOWED_MULTIVIEW_HEADS = Union[
-    MultiviewHeatmapCNNHead,
-]
-
-ALLOWED_MULTIVIEW_MULTIHEADS = Union[
-    MultiviewHeatmapCNNMultiHead,
-]
 
 # to ignore imports for sphix-autoapidoc
 __all__ = []
