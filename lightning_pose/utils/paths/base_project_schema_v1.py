5bae7c83e72259e3f30209302f168300b47842f1
Karan Sikka <karanssikka@gmail.com>
2025-11-06 00:56:03 -0500

    change to spec-based impl

--- lightning_pose/utils/paths/base_project_schema_v1.py
+++ lightning_pose/utils/paths/base_project_schema_v1.py
@@ -46,39 +46,12 @@ class BaseProjectSchemaV1(ABC):
         """Return the resource util for the given type."""
         raise NotImplementedError
 
-    # Unified public attributes for all schemas with precise generic types
-    @property
-    def videos(self) -> AbstractResourceUtil["VideoFileKey"]:
-        return self.for_(ResourceType.videos)
-
-    @property
-    def video_boxes(self) -> AbstractResourceUtil["VideoFileKey"]:
-        return self.for_(ResourceType.video_boxes)
-
-    @property
-    def frames(self) -> AbstractResourceUtil["FrameKey"]:
-        return self.for_(ResourceType.frames)
-
-    @property
-    def label_files(self) -> AbstractResourceUtil[tuple["LabelFileKey", Optional["ViewName"]]]:
-        return self.for_(ResourceType.label_files)
-
-    @property
-    def label_file_bboxes(self) -> AbstractResourceUtil[tuple["LabelFileKey", Optional["ViewName"]]]:
-        return self.for_(ResourceType.label_file_bboxes)
-
-    @property
-    def center_frames(self) -> AbstractResourceUtil["VideoFileKey"]:
-        return self.for_(ResourceType.center_frames)
-
-    @property
-    def session_calibrations(self) -> AbstractResourceUtil["SessionKey"]:
-        return self.for_(ResourceType.session_calibrations)
-
-    @property
-    def project_calibration(self) -> AbstractResourceUtil[bool]:
-        return self.for_(ResourceType.project_calibration)
-
-    @property
-    def calibration_backups(self) -> AbstractResourceUtil[tuple["SessionKey", int]]:
-        return self.for_(ResourceType.calibration_backups)
+    videos: AbstractResourceUtil["VideoFileKey"]
+    video_boxes: AbstractResourceUtil["VideoFileKey"]
+    frames: AbstractResourceUtil["FrameKey"]
+    label_files: AbstractResourceUtil[tuple["LabelFileKey", Optional["ViewName"]]]
+    label_file_bboxes: AbstractResourceUtil[tuple["LabelFileKey", Optional["ViewName"]]]
+    center_frames: AbstractResourceUtil["VideoFileKey"]
+    session_calibrations: AbstractResourceUtil["SessionKey"]
+    project_calibration: AbstractResourceUtil[bool]
+    calibration_backups: AbstractResourceUtil[tuple["SessionKey", int]]
