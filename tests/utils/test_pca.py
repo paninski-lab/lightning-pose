2dfa992830666364201df3c0e264813728a19baa
Matt Whiteway <themattinthehatt@gmail.com>
2026-05-20 13:48:34 -0400

    refact KeypointPCA tests

--- tests/utils/test_pca.py
+++ tests/utils/test_pca.py
@@ -11,205 +11,184 @@ def check_lists_equal(list_0: list, list_1: list) -> bool:
     return (len(list_0) == len(list_1)) and sorted(list_0) == sorted(list_1)
 
 
-def test_pca_keypoint_class_singleview(cfg, base_data_module_combined):
-    num_train_ims = int(
-        len(base_data_module_combined.dataset)
-        * base_data_module_combined.train_probability
-    )
-    num_keypoints = base_data_module_combined.dataset.num_keypoints
-    num_keypoints_for_pca = len(cfg.data.columns_for_singleview_pca)
+class TestKeypointPCA:
+    """Test the class KeypointPCA."""
 
-    # initialize an instance
-    kp_pca = KeypointPCA(
-        loss_type="pca_singleview",
-        data_module=base_data_module_combined,
-        components_to_keep=0.99,
-        empirical_epsilon_percentile=1.0,
-        columns_for_singleview_pca=cfg.data.columns_for_singleview_pca,
-    )
-
-    kp_pca._get_data()
-    assert kp_pca.data_arr.shape == (num_train_ims, 2 * num_keypoints)
-
-    kp_pca.data_arr = kp_pca._format_data(data_arr=kp_pca.data_arr)
-    assert kp_pca.data_arr.shape == (
-        num_train_ims,
-        2 * num_keypoints_for_pca,  # 2 coords per keypoint
-    )
-
-    # check that we have enough observations
-    kp_pca._check_data()  # raises ValueErrors if fails
-
-    # fit the pca model
-    kp_pca._fit_pca()
-    kp_pca._choose_n_components()
-    kp_pca.pca_prints()
-    kp_pca._set_parameter_dict()
-
-    check_lists_equal(
-        list(kp_pca.parameters.keys()),
-        ["mean", "kept_eigenvectors", "discarded_eigenvectors", "epsilon"],
-    )
-
-    # --------------------------------------
-    # test reprojection error computation
-    # --------------------------------------
-
-    # generate fake data
-    n_batches = 12
-    n_keypoints = 17
-    pred_keypoints = torch.rand(
-        size=(n_batches, n_keypoints * 2),
-        device="cpu",
-    )
-    # initialize an instance
-    singleview_pca = KeypointPCA(
-        loss_type="pca_singleview",
-        data_module=base_data_module_combined,
-        components_to_keep=6,
-        empirical_epsilon_percentile=1.0,
-        columns_for_singleview_pca=cfg.data.columns_for_singleview_pca,
-    )
-    singleview_pca()  # fit it to have all the parameters
-
-    # push pred_keypoints through the reformatter
-    data_arr = singleview_pca._format_data(data_arr=pred_keypoints)
-    # num selected keypoints in the cfg is 14, so we should have 14 * 2 columns
-    assert data_arr.shape == (n_batches, 14 * 2)
-
-    err = singleview_pca.compute_reprojection_error(data_arr=data_arr)
-    assert err.shape == (n_batches, 14)
-
-
-def test_pca_keypoint_class_median_centering(cfg, base_data_module_combined):
-    # initialize an instance
-    kp_pca = KeypointPCA(
-        loss_type="pca_singleview",
-        data_module=base_data_module_combined,
-        components_to_keep=0.99,
-        empirical_epsilon_percentile=1.0,
-        columns_for_singleview_pca=[0, 1, 2],
-        centering_method="median",
-    )
-
-    fmt_data_arr = kp_pca._format_data(
-        data_arr=torch.tensor(
-            [
-                [0, 1, 1, 2, 2, 3],  # Median of 1,2
-                [2, -100, 3, 4, 4, 100],  # Median of 3,4
-                [12, -90, 13, 14, 14, 110],  # 2nd vec translated by +(10,10)
-            ],
-            dtype=torch.float32,
+    def test_keypoint_pca_singleview(self, cfg, base_data_module_combined):
+        num_train_ims = int(
+            len(base_data_module_combined.dataset)
+            * base_data_module_combined.train_probability
         )
-    )
+        num_keypoints = base_data_module_combined.dataset.num_keypoints
+        num_keypoints_for_pca = len(cfg.data.columns_for_singleview_pca)
 
-    assert torch.equal(
-        fmt_data_arr,
-        torch.tensor(
-            [
-                [-1, -1, 0, 0, 1, 1],
-                [-1, -104, 0, 0, 1, 96],
-                [-1, -104, 0, 0, 1, 96],
-            ],
-            dtype=torch.float32,
-        ),
-    )
-
-
-def test_pca_keypoint_class_mean_centering(cfg, base_data_module_combined):
-    # initialize an instance
-    kp_pca = KeypointPCA(
-        loss_type="pca_singleview",
-        data_module=base_data_module_combined,
-        components_to_keep=0.99,
-        empirical_epsilon_percentile=1.0,
-        columns_for_singleview_pca=[0, 1, 2],
-        centering_method="mean",
-    )
-
-    fmt_data_arr = kp_pca._format_data(
-        data_arr=torch.tensor(
-            [
-                [0, 1, 1, 2, 2, 3],  # Mean of 1,2
-                [2, -100, 3, 6, 4, 100],  # Mean of 3, 2
-                [12, -90, 13, 16, 14, 110],  # 2nd vec translated by +(10,10)
-            ],
-            dtype=torch.float32,
+        kp_pca = KeypointPCA(
+            loss_type="pca_singleview",
+            data_module=base_data_module_combined,
+            components_to_keep=0.99,
+            empirical_epsilon_percentile=1.0,
+            columns_for_singleview_pca=cfg.data.columns_for_singleview_pca,
         )
-    )
 
-    assert torch.equal(
-        fmt_data_arr,
-        torch.tensor(
-            [
-                [-1, -1, 0, 0, 1, 1],
-                [-1, -102, 0, 4, 1, 98],
-                [-1, -102, 0, 4, 1, 98],
-            ],
-            dtype=torch.float32,
-        ),
-    )
+        kp_pca._get_data()
+        assert kp_pca.data_arr.shape == (num_train_ims, 2 * num_keypoints)
 
+        kp_pca.data_arr = kp_pca._format_data(data_arr=kp_pca.data_arr)
+        assert kp_pca.data_arr.shape == (
+            num_train_ims,
+            2 * num_keypoints_for_pca,  # 2 coords per keypoint
+        )
 
-def test_pca_keypoint_class_multiview(
-    cfg,
-    base_data_module_combined,
-    cfg_multiview,
-    multiview_heatmap_data_module_combined,
-):
+        kp_pca._check_data()  # raises ValueErrors if fails
 
-    num_train_ims = (
-        len(base_data_module_combined.dataset)
-        * base_data_module_combined.train_probability
-    )
-    num_keypoints = base_data_module_combined.dataset.num_keypoints
-    num_keypoints_both_views = 7
+        kp_pca._fit_pca()
+        kp_pca._choose_n_components()
+        kp_pca.pca_prints()
+        kp_pca._set_parameter_dict()
 
-    # initialize an instance
-    kp_pca = KeypointPCA(
-        loss_type="pca_multiview",
-        data_module=base_data_module_combined,
-        components_to_keep=3,
-        empirical_epsilon_percentile=0.3,
-        mirrored_column_matches=cfg.data.mirrored_column_matches,
-    )
+        check_lists_equal(
+            list(kp_pca.parameters.keys()),
+            ["mean", "kept_eigenvectors", "discarded_eigenvectors", "epsilon"],
+        )
 
-    kp_pca._get_data()
-    assert kp_pca.data_arr.shape == (num_train_ims, 2 * num_keypoints)
+        # test reprojection error computation
+        n_batches = 12
+        n_keypoints = 17
+        pred_keypoints = torch.rand(size=(n_batches, n_keypoints * 2), device="cpu")
+        singleview_pca = KeypointPCA(
+            loss_type="pca_singleview",
+            data_module=base_data_module_combined,
+            components_to_keep=6,
+            empirical_epsilon_percentile=1.0,
+            columns_for_singleview_pca=cfg.data.columns_for_singleview_pca,
+        )
+        singleview_pca()
 
-    kp_pca.data_arr = kp_pca._format_data(data_arr=kp_pca.data_arr)
-    assert kp_pca.data_arr.shape == (
-        num_keypoints_both_views * num_train_ims,
-        4,  # 4 coords per keypoint (2 views)
-    )
+        data_arr = singleview_pca._format_data(data_arr=pred_keypoints)
+        # num selected keypoints in the cfg is 14, so we should have 14 * 2 columns
+        assert data_arr.shape == (n_batches, 14 * 2)
 
-    # check that we have enough observations
-    kp_pca._check_data()  # raises ValueErrors if fails
+        err = singleview_pca.compute_reprojection_error(data_arr=data_arr)
+        assert err.shape == (n_batches, 14)
 
-    # fit the pca model
-    kp_pca._fit_pca()
+    def test_keypoint_pca_median_centering(self, cfg, base_data_module_combined):
+        kp_pca = KeypointPCA(
+            loss_type="pca_singleview",
+            data_module=base_data_module_combined,
+            components_to_keep=0.99,
+            empirical_epsilon_percentile=1.0,
+            columns_for_singleview_pca=[0, 1, 2],
+            centering_method="median",
+        )
 
-    # we specified 0.9 components to keep but we'll take 3
-    kp_pca._choose_n_components()
+        fmt_data_arr = kp_pca._format_data(
+            data_arr=torch.tensor(
+                [
+                    [0, 1, 1, 2, 2, 3],  # Median of 1,2
+                    [2, -100, 3, 4, 4, 100],  # Median of 3,4
+                    [12, -90, 13, 14, 14, 110],  # 2nd vec translated by +(10,10)
+                ],
+                dtype=torch.float32,
+            )
+        )
 
-    kp_pca.pca_prints()
+        assert torch.equal(
+            fmt_data_arr,
+            torch.tensor(
+                [
+                    [-1, -1, 0, 0, 1, 1],
+                    [-1, -104, 0, 0, 1, 96],
+                    [-1, -104, 0, 0, 1, 96],
+                ],
+                dtype=torch.float32,
+            ),
+        )
 
-    kp_pca._set_parameter_dict()
+    def test_keypoint_pca_mean_centering(self, cfg, base_data_module_combined):
+        kp_pca = KeypointPCA(
+            loss_type="pca_singleview",
+            data_module=base_data_module_combined,
+            components_to_keep=0.99,
+            empirical_epsilon_percentile=1.0,
+            columns_for_singleview_pca=[0, 1, 2],
+            centering_method="mean",
+        )
 
-    check_lists_equal(
-        list(kp_pca.parameters.keys()),
-        ["mean", "kept_eigenvectors", "discarded_eigenvectors", "epsilon"],
-    )
+        fmt_data_arr = kp_pca._format_data(
+            data_arr=torch.tensor(
+                [
+                    [0, 1, 1, 2, 2, 3],  # Mean of 1,2
+                    [2, -100, 3, 6, 4, 100],  # Mean of 3, 2
+                    [12, -90, 13, 16, 14, 110],  # 2nd vec translated by +(10,10)
+                ],
+                dtype=torch.float32,
+            )
+        )
 
-    # make sure we don't get errors when using a true multiview dataset
-    kp_pca_2 = KeypointPCA(
-        loss_type="pca_multiview",
-        data_module=multiview_heatmap_data_module_combined,
-        components_to_keep=3,
-        empirical_epsilon_percentile=0.3,
-        mirrored_column_matches=cfg_multiview.data.mirrored_column_matches,
-    )
-    kp_pca_2()
+        assert torch.equal(
+            fmt_data_arr,
+            torch.tensor(
+                [
+                    [-1, -1, 0, 0, 1, 1],
+                    [-1, -102, 0, 4, 1, 98],
+                    [-1, -102, 0, 4, 1, 98],
+                ],
+                dtype=torch.float32,
+            ),
+        )
+
+    def test_keypoint_pca_multiview(
+        self,
+        cfg,
+        base_data_module_combined,
+        cfg_multiview,
+        multiview_heatmap_data_module_combined,
+    ):
+        num_train_ims = (
+            len(base_data_module_combined.dataset)
+            * base_data_module_combined.train_probability
+        )
+        num_keypoints = base_data_module_combined.dataset.num_keypoints
+        num_keypoints_both_views = 7
+
+        kp_pca = KeypointPCA(
+            loss_type="pca_multiview",
+            data_module=base_data_module_combined,
+            components_to_keep=3,
+            empirical_epsilon_percentile=0.3,
+            mirrored_column_matches=cfg.data.mirrored_column_matches,
+        )
+
+        kp_pca._get_data()
+        assert kp_pca.data_arr.shape == (num_train_ims, 2 * num_keypoints)
+
+        kp_pca.data_arr = kp_pca._format_data(data_arr=kp_pca.data_arr)
+        assert kp_pca.data_arr.shape == (
+            num_keypoints_both_views * num_train_ims,
+            4,  # 4 coords per keypoint (2 views)
+        )
+
+        kp_pca._check_data()  # raises ValueErrors if fails
+
+        kp_pca._fit_pca()
+        kp_pca._choose_n_components()
+        kp_pca.pca_prints()
+        kp_pca._set_parameter_dict()
+
+        check_lists_equal(
+            list(kp_pca.parameters.keys()),
+            ["mean", "kept_eigenvectors", "discarded_eigenvectors", "epsilon"],
+        )
+
+        # make sure we don't get errors when using a true multiview dataset
+        kp_pca_2 = KeypointPCA(
+            loss_type="pca_multiview",
+            data_module=multiview_heatmap_data_module_combined,
+            components_to_keep=3,
+            empirical_epsilon_percentile=0.3,
+            mirrored_column_matches=cfg_multiview.data.mirrored_column_matches,
+        )
+        kp_pca_2()
 
 
 def test_nan_pca():
